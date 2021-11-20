d47f49ec33be94a249ef1ddd8ef7b1ccd8e40d3d
NotZeetaa <rodrigo2005contente@gmail.com>
2021-11-20 23:11:42 +0000

    Revert "ci: Update"
    This reverts commit 5cec4dfb73ec8e0b825b7b8bd1986911fe1b61f4.

--- .circleci/build2.sh
+++ .circleci/build2.sh
@@ -2,7 +2,7 @@
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/NotZeetaa/Flashable_Zip_lmi.git -b alioth AnyKernel2
 echo "Done"
-IMAGE=$(pwd)/out/arch/arm64/boot/Image
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 KERNEL_DIR=$(pwd)
