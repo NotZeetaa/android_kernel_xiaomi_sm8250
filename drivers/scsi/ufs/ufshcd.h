c350766defbfa6a95f10ec04a953dacefe96e719
NotZeetaa <rodrigo2005contente@gmail.com>
2021-11-26 19:30:09 +0000

    Revert "scsi: ufs: Use async operation for hibern8 operation"
    This reverts commit 4db096729310d9d76671f6d8aa70b0a754f12e3a.

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -525,7 +525,6 @@ enum ufshcd_hibern8_on_idle_state {
  * @delay_attr: sysfs attribute to control delay_attr
  * @enable_attr: sysfs attribute to enable/disable hibern8 on idle
  * @is_enabled: Indicates the current status of hibern8
- * @enable_mutex: protect sys node race from multithread access
  */
 struct ufs_hibern8_on_idle {
 	struct delayed_work enter_work;
@@ -537,7 +536,6 @@ struct ufs_hibern8_on_idle {
 	struct device_attribute delay_attr;
 	struct device_attribute enable_attr;
 	bool is_enabled;
-	struct mutex enable_mutex;
 };
 
 /**
@@ -1010,7 +1008,6 @@ struct ufs_hba {
 	struct work_struct eh_work;
 	struct work_struct eeh_work;
 	struct work_struct rls_work;
-	struct work_struct hibern8_on_idle_enable_work;
 
 	/* HBA Errors */
 	u32 errors;
