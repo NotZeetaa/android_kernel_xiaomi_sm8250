5899b9d63dc98f31062dd93f2d431ac680efe2e1
NotZeetaa <rodrigo2005contente@gmail.com>
2022-01-21 20:41:41 +0000

    configs: Disable Schedhorizon
    * For now
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 15 17:44:49 WET 2022"
+#define LINUX_COMPILE_BY "NotZeeta"
+#define LINUX_COMPILE_HOST "ArchLinux"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.19.225-neXus-lmi-V11--g2d3f699122b1"
